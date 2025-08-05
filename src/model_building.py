d46b2a2dc2822c3f5054993a04ebcc915c507d69
md mohsin <siam.mohsin2005@gmail.com>
2025-08-05 12:17:54 +0600

    Update pipeline

--- src/model_building.py
+++ src/model_building.py
@@ -7,7 +7,7 @@ train_data = pd.read_csv('data/features/train.csv')
 x_train = train_data.drop('Survived', axis=1)
 y_train = train_data['Survived']
 
-model = RandomForestClassifier(n_estimators=145, criterion='entropy')
+model = RandomForestClassifier(criterion='entropy', max_depth=5, n_estimators=50)
 model.fit(x_train, y_train)
 
 pickle.dump(model, open('model.pkl', 'wb'))
