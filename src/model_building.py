0ebdfd899a9f3917260ac7d21f24fe4215698b9d
md mohsin <siam.mohsin2005@gmail.com>
2025-08-05 12:44:56 +0600

    update dvc pipeline

--- src/model_building.py
+++ src/model_building.py
@@ -1,13 +1,21 @@
 import pandas as pd
-from sklearn.ensemble import RandomForestClassifier
 import pickle
+from xgboost import XGBClassifier
+import yaml
 
 train_data = pd.read_csv('data/features/train.csv')
 
 x_train = train_data.drop('Survived', axis=1)
 y_train = train_data['Survived']
 
-model = RandomForestClassifier(criterion='entropy', max_depth=5, n_estimators=50)
+# Load Parameter
+with open('params.yaml', 'r') as file:
+    params = yaml.safe_load(file)
+    
+model_params = params['xgboost_params']
+
+# Train Model
+model = XGBClassifier(**model_params)
 model.fit(x_train, y_train)
 
 pickle.dump(model, open('model.pkl', 'wb'))
